c34c21bcb0220c76f30bbabe969c9c5e97d13748
Marcos Paulo Machado Azevedo <marcospaulo.dev@hotmail.com>
2023-09-21 13:20:00 -0300

    Alteração no envio de e-mail

--- src/resources/setEmail.py
+++ src/resources/setEmail.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, HTTPException, UploadFile, File, Form
+from fastapi import FastAPI, APIRouter, HTTPException, UploadFile, File, Form, BackgroundTasks
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -23,18 +23,14 @@ smtp_server.login(SMTP_USER, SMTP_PASSWORD)
 
 @router.post("/")
 async def send_email_endpoint(
+    background_tasks: BackgroundTasks,
     subject: str = Form(...),
     to_email: str = Form(...),
     html_content: str = Form(...),
     attachments: List[UploadFile] = File(...)
 ):
-    try:
-        await asyncio.gather(
-            send_email(subject, to_email, html_content, attachments)
-        )
-        return {"status": 200, "message": "Email sent successfully!"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+    background_tasks.add_task(sendemail, subject, to_email, html_content, attachments)
+    return 'ok'
 
 async def send_email(subject: str, to_email: str, html_content: str, attachments: List[UploadFile] = []):
     msg = MIMEMultipart('alternative')
@@ -54,3 +50,16 @@ async def send_email(subject: str, to_email: str, html_content: str, attachments
 
     # Envie o email usando a conexão SMTP persistente
     smtp_server.sendmail(SMTP_USER, to_email, msg.as_string())
+    
+    
+async def sendemail(subject,
+            to_email,
+            html_content,
+            attachments):
+    try:
+        await asyncio.gather(
+            send_email(subject, to_email, html_content, attachments)
+        )
+        return {"status": 200, "message": "Email sent successfully!"}
+    except Exception as e:
+        pass
